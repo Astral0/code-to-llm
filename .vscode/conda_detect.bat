523893e76bfadd1b04b601c07c6425054e2ef15a
Astral0 <Astral0@protonmail.com>
2025-07-30 10:59:34 +0200

    feat: Add VS Code settings for auto conda activation
    This commit introduces a new feature to streamline the development setup within Visual Studio Code on Windows by automatically activating the correct Conda environment.
    
    - A new batch script `conda_detect.bat` is added to locate and activate the 'code2llm' Conda environment. It searches through common installation directories for Anaconda and Miniconda.
    - VS Code workspace settings (`.vscode/settings.json`) are configured to create a new terminal profile named "Code2LLM".
    - This new profile is set as the default for Windows, ensuring that any new integrated terminal automatically runs the detection script and activates the appropriate environment.
    - The `.cjs` file extension has been added to the `extension_blacklist` in the `config.ini.template` to prevent these files from being processed.

---
+++ .vscode/conda_detect.bat
@@ -0,0 +1,29 @@
+@echo off
+
+REM Test des emplacements un par un
+if exist "C:\UTILS\miniconda3\Scripts\conda.exe" (
+    call "C:\UTILS\miniconda3\Scripts\activate.bat" "C:\UTILS\miniconda3" && conda activate code2llm
+    goto :eof
+)
+
+if exist "C:\APPLIS\miniconda3"\Scripts\conda.exe" (
+    call "C:\APPLIS\miniconda3"\Scripts\activate.bat" "C:\UTILS\miniconda3" && conda activate code2llm
+    goto :eof
+)
+
+if exist "%USERPROFILE%\anaconda3\Scripts\conda.exe" (
+    call "%USERPROFILE%\anaconda3\Scripts\activate.bat" "%USERPROFILE%\anaconda3" && conda activate code2llm
+    goto :eof
+)
+
+if exist "%USERPROFILE%\miniconda3\Scripts\conda.exe" (
+    call "%USERPROFILE%\miniconda3\Scripts\activate.bat" "%USERPROFILE%\miniconda3" && conda activate code2llm
+    goto :eof
+)
+
+if exist "C:\ProgramData\Anaconda3\Scripts\conda.exe" (
+    call "C:\ProgramData\Anaconda3\Scripts\activate.bat" "C:\ProgramData\Anaconda3" && conda activate code2llm
+    goto :eof
+)
+
+echo Conda non trouve - environnement par defaut
